c4336d01b2fd9e632d06b0fb6bd361c614f97df4
Marek Küthe <m.k@mk16.de>
2024-03-23 17:54:03 +0000

    Add test

--- src/nodereply.cpp
+++ src/nodereply.cpp
@@ -130,7 +130,8 @@ std::string NodeReply::to_packet() const
             }
             Tins::PDU::serialization_type serialized_receiving_packet =
                 receiving_ipv6.serialize();
-            serialized_receiving_packet.resize(1000);
+            if (serialized_receiving_packet.size() > 1000)
+                serialized_receiving_packet.resize(1000);
 
             switch (this->_type)
             {
